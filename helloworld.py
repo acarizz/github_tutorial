8eb353cedbcf83f7eab80d57623cc0a631ff89a7
Abril Zapata <abrilcarizz@gmail.com>
2025-07-07 00:40:32 -0700

    Add median function

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,9 @@
-print ('helloworld.py')
+print('helloworld')
+
+def calculate_median(numbers):
+    sorted_numbers = sorted(numbers)
+    n = len(sorted_numbers)
+    if n % 2 == 0:
+        return (sorted_numbers[n//2 - 1] + sorted_numbers[n//2]) / 2
+    else:
+        return sorted_numbers[n//2]
